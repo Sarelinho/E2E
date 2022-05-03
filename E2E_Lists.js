f2cffeb3d2920a742397c62495570032b15b3278
EitanCher <102179966+EitanCher@users.noreply.github.com>
2022-05-03 08:15:46 +0300

    Update E2E_Lists.js

--- E2E_Lists.js
+++ E2E_Lists.js
@@ -1,32 +1,34 @@
 function Dish(name, ingred1, ingred2, ingred3, ingred4, ingred5) {
-  this.DishName = name;
-  this.Ingredient1 = ingred1;
-  this.Ingredient2 = ingred2;
-  this.Ingredient3 = ingred3;
-  this.Ingredient4 = ingred4;
-  this.Ingredient5 = ingred5;
+  this.DishName = "name";
+  this.Ingredient1 = "none";
+  this.Ingredient2 = "none";
+  this.Ingredient3 = "none";
+  this.Ingredient4 = "none";
+  this.Ingredient5 = "none";
 }
 
 const DishList = [
 	Shakshuka, Pancake
 ];
 
+const myPath = "";
+
 // List of Dishes
 
-const Shakshuka = new Dish("", ing_tomato, ing_onion, ing_parsley, ing_eggs, ing_paprika);
-const Pancake = new Dish("", ing_eggs, ing_milk, ing_flour, ing_sugar, ing_maple);
+const Shakshuka = new Dish("שקשוקה", ing_tomato, ing_onion, ing_parsley, ing_eggs, ing_paprika);
+const Pancake = new Dish("פנקייק", ing_eggs, ing_milk, ing_flour, ing_sugar, ing_maple);
 
 // List of Ingredients
 
-const ing_eggs = "";
-const ing_flour = "";
-const ing_lentils = "";
-const ing_maple = "";
-const ing_milk = "";
-const ing_onion = "";
-const ing_paprika = "";
-const ing_parsley = "";
-const ing_rice = "";
-const ing_sugar = ""
-const ing_tomato = "";
+const ing_eggs = myPath + "";
+const ing_flour = myPath + "";
+const ing_lentils = myPath + "";
+const ing_maple = myPath + "";
+const ing_milk = myPath + "";
+const ing_onion = myPath + "";
+const ing_paprika = myPath + "";
+const ing_parsley = myPath + "";
+const ing_rice = myPath + "";
+const ing_sugar = myPath + ""
+const ing_tomato = myPath + "";
 
