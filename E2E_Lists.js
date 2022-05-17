bce98951bbc8cd3e775578871706c02664ecca97
danieln1998 <101664115+danieln1998@users.noreply.github.com>
2022-05-17 20:01:13 +0300

    Update E2E_Lists.js

--- E2E_Lists.js
+++ E2E_Lists.js
@@ -16,19 +16,58 @@ const myPath = "";
 // List of Dishes
 
 const Pancake = new Dish("פנקייק", ing_eggs, ing_milk, ing_flour, ing_sugar, ing_maple);
-const Shakshuka = new Dish("שקשוקה", ing_tomato, ing_onion, ing_parsley, ing_eggs, ing_paprika);
-
-// List of Ingredients
-
-const ing_eggs = myPath + "";
-const ing_flour = myPath + "";
-const ing_lentils = myPath + "";
-const ing_maple = myPath + "";
-const ing_milk = myPath + "";
-const ing_onion = myPath + "";
-const ing_paprika = myPath + "";
-const ing_parsley = myPath + "";
-const ing_rice = myPath + "";
-const ing_sugar = myPath + ""
-const ing_tomato = myPath + "";
-
+  const Shakshuka = new Dish("שקשוקה", ing_tomato, ing_onion, ing_parsley, ing_eggs, ing_paprika);
+  const Kuba_selek=new Dish ("קובה סלק", ing_selek, ing_solet, ing_celery,ing_meat,ing_Tomatopaste ) ;
+  const Knafe =new Dish ("כנאפה", ing_cheese, ing_Kaddif , ing_roseWater,ing_sugarWater,ing_fistuk ) ;
+  const Ingera =new Dish ("אינג'רה", ing_taf, ing_siga , ing_tapuahAdama,ing_chicken,ing_eggs ) ;
+  const Makluba =new Dish ("מקלובה",ing_orez, ing_gezer ,  ing_kruvit,ing_meat,ing_tapuahAdama ) ;
+  const Memulaim =new Dish ("ממולאים",ing_orez, ing_gamba ,  ing_tomato,ing_meat,ing_onion ) ;
+  const Musaka =new Dish ("מוסקה",ing_hazil , ing_onion ,  ing_tomato,ing_meat,ing_parsley) ;
+  const Matbuha =new Dish ("מטבוחה",ing_shum , ing_pilplelHarif ,  ing_tomato,ing_shemen,ing_Tomatopaste) ;
+  const Muskus =new Dish ("קוסקוס",ing_solet ,  ing_dlahat ,  ing_chicken,ing_gezer ,ing_kruv) ;
+  const FishandChips =new Dish ("פיש&צ'יפס",ing_fish ,  ing_bira , ing_tapuahAdama,ing_shemen ,ing_flour) ;
+  const FishMarokae =new Dish ("דג מרוקאי",ing_fish ,  ing_paprika ,ing_pilplelHarif,ing_shemen , ing_gamba) ;
+  const Arayes =new Dish ("עראייס",ing_pita , ing_shemZait ,ing_meat,ing_parsley , ing_onion) ;
+  
+  
+  
+  // List of Ingredients
+  
+  const ing_eggs = myPath + "";
+  const ing_flour = myPath + "";
+  const ing_lentils = myPath + "";
+  const ing_maple = myPath + "";
+  const ing_milk = myPath + "";
+  const ing_onion = myPath + "";
+  const ing_paprika = myPath + "";
+  const ing_parsley = myPath + "";
+  const ing_sugar = myPath + ""
+  const ing_tomato = myPath + "";
+  const ing_selek = myPath + "";
+  const ing_solet = myPath + "";
+  const ing_celery = myPath + "";
+  const ing_meat = myPath + ""; 
+  const ing_Tomatopaste= myPath + "";   
+  const ing_cheese = myPath + ""; 
+  const ing_Kaddif = myPath + ""; 
+  const ing_roseWater = myPath + "";   
+  const ing_sugarWater = myPath + ""; 
+  const ing_fistuk = myPath + ""; 
+  const ing_taf = myPath + "";
+  const ing_siga = myPath + "";
+  const ing_tapuahAdama= myPath + "";
+  const ing_chicken = myPath + "";
+  const ing_orez = myPath + "";
+  const ing_gezer = myPath + "";
+  const ing_kruvit = myPath + "";
+  const ing_gamba = myPath + "";
+  const ing_hazil = myPath + "";
+  const ing_shum = myPath + "";
+  const ing_pilplelHarif= myPath + "";
+  const ing_shemen= myPath + "";
+  const ing_dlahat= myPath + "";
+  const ing_kruv= myPath + "";
+  const ing_fish= myPath + "";
+  const ing_bira= myPath + "";
+  const ing_pita= myPath + "";
+  const ing_shemZait= myPath + "";
