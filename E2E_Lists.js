3844d067fde082112988cec08d95bd06f5081dc3
EitanCher <102179966+EitanCher@users.noreply.github.com>
2022-05-29 02:14:08 +0300

    Update E2E_Lists.js

--- E2E_Lists.js
+++ E2E_Lists.js
@@ -8,56 +8,56 @@ function Dish(name, ingred1, ingred2, ingred3, ingred4, ingred5) {
 }
   
 const DishList = [
-    Arayes, Artichoke, Borekas, Caprese, Cheesecake, CornPie, DateCookies, Doughnuts, Falafel, FishandChips, 
-    FishSticks, Guacamole, GormeySbazi, Hamin, Hrira, Ingera, Kholodets, Knafe, Kneidlech, Kuba_selek, 
-    Kuskus, Mafrum, Makluba, MaroccanFish, Matbuha, MeoravYerushalmi, Memulaim, Mjadara, Musaka, Olivier, 
-    Omelet, OzneyHaman, Pancake, PastaAlfredo, Pizza, Shakshuka, Schnitzel, StirFriedSalad, ThaiStirFried, Vareniki
+    Arayes, Artichoke, Borekas, Borsht, Caprese, Cheesecake, CornPie, DateCookies, Doughnuts, Falafel, 
+    FishandChips, FishSticks, Guacamole, GormeySbazi, Hamin, Hrira, Ingera, Kholodets, Knafe, Kneidlech, 
+    Kuba_selek, Kuskus, Mafrum, Makluba, MaroccanFish, Matbuha, MeoravYerushalmi, Memulaim, Mjadara, Musaka, 
+    Olivier, Omelet, OzneyHaman, Pancake, PastaAlfredo, Pizza, Shakshuka, Schnitzel, StirFriedSalad, ThaiStirFried
 ];
   
 const myPath = "";
   
 // List of Dishes
   
-const Arayes =      new Dish("עראייס",      ing_pita, ing_oliveoil, ing_meat, ing_parsley, ing_onion);
-const Artichoke =   new Dish("ארטישוק ממולא", ing_Artishok, ing_Silan, ing_garlic, ing_celery, ing_BasarTahun);
-const Borekas =     new Dish("בורקס בשר",   ing_Bazek_filo, ing_BasarTahun, ing_tomato, ing_Baharat, ing_oliveoil);
-const Caprese =     new Dish("סלט קפרז\'ה",  ing_balsamic, ing_mozzarella, ing_basil, ing_tomato, ing_coarsesalt);
-const Cheesecake =  new Dish("עוגת גבינה",  ing_eggs, ing_flour, ing_GvinaLevana, ing_SugarVanil, ing_InstantPudingVanil);
-const CornPie =     new Dish("פשטידת תירס", ing_butter, ing_corn, ing_cheese, ing_flour, ing_cream);
-const DateCookies = new Dish("עוגיות תמרים", ing_MimrahTmarim, ing_flour, ing_eggs, ing_Mahmha, ing_AvkatAfiya);
-const Doughnuts =   new Dish("סופגניה",     ing_sugar, ing_flour, ing_Riba, ing_AvkatSugar, ing_oil);
-const Falafel =     new Dish("פלאפל",       ing_Humus, ing_onion, ing_parsley, ing_garlic, ing_hotpepper);
-const FishandChips = new Dish("פיש\&צ\'יפס", ing_fish, ing_beer, ing_potato, ing_oil, ing_flour);
-const FishSticks =  new Dish("קציצות דגים", ing_fish, ing_eggs, ing_onion, ing_tomatopaste, ing_semolina);
-const GormeySbazi = new Dish("גורמי סבזי",  ing_turmeric, ing_bean ,ing_persianlemon, ing_onion, ing_parsley);
-const Guacamole =   new Dish("גוואקאמולי",  ing_avocado, ing_lemon, ing_garlic, ing_tomato, ing_oliveoil);
-const Hamin =       new Dish("חמין",        ing_eggs, ing_Hita, ing_potato, ing_bean, ing_rice);
-const Hrira =       new Dish("מרק חרירה",   ing_meat, ing_lentils, ing_onion, ing_tomato, ing_celery);
-const Ingera =      new Dish("אינג'רה",     ing_taf, ing_siga, ing_potato, ing_chicken, ing_eggs);
-const Kholodets =   new Dish("רגל קרושה",   ing_beefarm, ing_onion, ing_celery, ing_garlic, ing_carrot);
-const Knafe =       new Dish("כנאפה",       ing_cheese, ing_kaddif, ing_rosewater, ing_sugarwater, ing_pistachio);
-const Kneidlech =   new Dish("קניידלך",     ing_KemahMaza, ing_eggs, ing_oil, ing_MarakOf, ing_Melah);
-const Kuba_selek =  new Dish("קובה סלק",    ing_beet, ing_semolina, ing_celery, ing_meat, ing_tomatopaste);
-const Kuskus =      new Dish("קוסקוס",      ing_semolina, ing_pumpkin, ing_chicken, ing_carrot ,ing_cabbage);
-const Mafrum =      new Dish("מפרום",       ing_Burgul, ing_cauliflower, ing_onion, ing_BasarTahun, ing_parsley);
-const Makluba =     new Dish("מקלובה",      ing_rice, ing_carrot, ing_cauliflower, ing_meat, ing_potato);
-const MaroccanFish = new Dish ("דג מרוקאי", ing_fish, ing_paprika, ing_hotpepper, ing_oil, ing_gamba);
-const Matbuha =     new Dish("מטבוחה",      ing_garlic, ing_hotpepper, ing_tomato, ing_oil, ing_tomatopaste);
-const Memulaim =    new Dish("ממולאים",     ing_rice, ing_gamba, ing_tomato, ing_meat, ing_onion);
-const MeoravYerushalmi = new Dish("מעורב ירושלמי", ing_spleen, ing_chickenbreast, ing_hawayeg, ing_turmeric, ing_onion);
-const Mjadara =     new Dish("מג\'דרה",      ing_rice, ing_dummy, ing_onion, ing_dummy, ing_lentils);
-const Musaka =      new Dish("מוסקה",       ing_eggplant, ing_onion, ing_tomato, ing_meat, ing_parsley);
-const Olivier =     new Dish("סלט אוליביה", ing_potato, ing_carrot, ing_eggs, ing_chickenbreast, ing_mayonnaise);
-const Omelet =      new Dish("שניצל",       ing_eggs, ing_oliveoil, ing_onion, ing_parsley, ing_Melah);
-const OzneyHaman=   new Dish("אוזני המן",   ing_yeast, ing_poppy, ing_chokolate, ing_flour, ing_butter);
-const Pancake =     new Dish("פנקייק",      ing_eggs, ing_milk, ing_flour, ing_sugar, ing_maple);
-const PastaAlfredo = new Dish("פסטה אלפרדו",ing_spagetti, ing_cream, ing_garlicpowder, ing_parmesan, ing_onion);
-const Pizza =       new Dish("פיצה",        ing_flour, ing_yeast, ing_tomatopaste, ing_cheese, ing_oliveoil);
-const Shakshuka =   new Dish("שקשוקה",      ing_tomato, ing_onion, ing_parsley, ing_eggs, ing_paprika);
-const Schnitzel =   new Dish("שניצל",       ing_eggs, ing_PerureyLehem, ing_chickenbreast, ing_sesame, ing_HardalDizon);
-const StirFriedSalad = new Dish("מוקפץ",    ing_Pasta, ing_chickenbreast, ing_carrot, ing_onion, ing_gamba);
-const ThaiStirFried = new Dish("מוקפץ תאילנדי", ing_chilesouce, ing_soysauce, ing_sesameoil, ing_chicken, ing_gamba);
-const Vareniki =    new Dish("וריניקי",     ing_potato, ing_cabbage, ing_Riba, ing_BasarTahun, ing_Pitryot);
+const Arayes            = new Dish("עראייס", ing_pita, ing_oliveoil, ing_meat, ing_parsley, ing_onion);
+const Artichoke         = new Dish("ארטישוק ממולא", ing_Artishok, ing_Silan, ing_garlic, ing_celery, ing_BasarTahun);
+const Borekas           = new Dish("בורקס בשר", ing_Bazek_filo, ing_BasarTahun, ing_tomato, ing_Baharat, ing_oliveoil);
+const Caprese           = new Dish("סלט קפרז\'ה", ing_balsamic, ing_mozzarella, ing_basil, ing_tomato, ing_coarsesalt);
+const Cheesecake        = new Dish("עוגת גבינה", ing_eggs, ing_flour, ing_GvinaLevana, ing_SugarVanil, ing_InstantPudingVanil);
+const CornPie           = new Dish("פשטידת תירס", ing_butter, ing_corn, ing_cheese, ing_flour, ing_cream);
+const DateCookies       = new Dish("עוגיות תמרים", ing_MimrahTmarim, ing_flour, ing_eggs, ing_Mahmha, ing_AvkatAfiya);
+const Doughnuts         = new Dish("סופגניה", ing_sugar, ing_flour, ing_Riba, ing_AvkatSugar, ing_oil);
+const Falafel           = new Dish("פלאפל", ing_Humus, ing_onion, ing_parsley, ing_garlic, ing_hotpepper);
+const FishandChips      = new Dish("פיש\&צ\'יפס", ing_fish, ing_beer, ing_potato, ing_oil, ing_flour);
+const FishSticks        = new Dish("קציצות דגים", ing_fish, ing_eggs, ing_onion, ing_tomatopaste, ing_semolina);
+const GormeySbazi       = new Dish("גורמי סבזי", ing_turmeric, ing_bean ,ing_persianlemon, ing_onion, ing_parsley);
+const Guacamole         = new Dish("גוואקאמולי", ing_avocado, ing_lemon, ing_garlic, ing_tomato, ing_oliveoil);
+const Hamin             = new Dish("חמין", ing_eggs, ing_Hita, ing_potato, ing_bean, ing_rice);
+const Hrira             = new Dish("מרק חרירה", ing_meat, ing_lentils, ing_onion, ing_tomato, ing_celery);
+const Ingera            = new Dish("אינג'רה", ing_taf, ing_siga, ing_potato, ing_chicken, ing_eggs);
+const Kholodets         = new Dish("רגל קרושה", ing_beefarm, ing_onion, ing_celery, ing_garlic, ing_carrot);
+const Knafe             = new Dish("כנאפה", ing_cheese, ing_kaddif, ing_rosewater, ing_sugarwater, ing_pistachio);
+const Kneidlech         = new Dish("קניידלך", ing_KemahMaza, ing_eggs, ing_oil, ing_MarakOf, ing_dummy);
+const Kuba_selek        = new Dish("קובה סלק", ing_beet, ing_semolina, ing_celery, ing_meat, ing_tomatopaste);
+const Kuskus            = new Dish("קוסקוס", ing_semolina, ing_pumpkin, ing_chicken, ing_carrot ,ing_cabbage);
+const Mafrum            = new Dish("מפרום", ing_Burgul, ing_cauliflower, ing_onion, ing_BasarTahun, ing_parsley);
+const Makluba           = new Dish("מקלובה", ing_rice, ing_carrot, ing_cauliflower, ing_meat, ing_potato);
+const MaroccanFish      = new Dish("דג מרוקאי", ing_fish, ing_paprika, ing_hotpepper, ing_oil, ing_gamba);
+const Matbuha           = new Dish("מטבוחה", ing_garlic, ing_hotpepper, ing_tomato, ing_oil, ing_tomatopaste);
+const Memulaim          = new Dish("ממולאים", ing_rice, ing_gamba, ing_tomato, ing_meat, ing_onion);
+const Mjadara           = new Dish("מג\'דרה", ing_rice, ing_dummy, ing_onion, ing_dummy, ing_lentils);
+const Musaka            = new Dish("מוסקה", ing_eggplant, ing_onion, ing_tomato, ing_meat, ing_parsley);
+const Olivier           = new Dish("סלט אוליביה", ing_potato, ing_carrot, ing_eggs, ing_chickenbreast, ing_mayonnaise);
+const Omelet            = new Dish("חביתת ירק", ing_eggs, ing_oliveoil, ing_onion, ing_parsley, ing_dummy);
+const OzneyHaman        = new Dish("אוזני המן", ing_yeast, ing_poppy, ing_chokolate, ing_flour, ing_butter);
+const Pancake           = new Dish("פנקייק", ing_eggs, ing_milk, ing_flour, ing_sugar, ing_maple);
+const PastaAlfredo      = new Dish("פסטה אלפרדו",ing_spagetti, ing_cream, ing_garlicpowder, ing_parmesan, ing_onion);
+const Pizza             = new Dish("פיצה", ing_flour, ing_yeast, ing_tomatopaste, ing_cheese, ing_oliveoil);
+const Shakshuka         = new Dish("שקשוקה", ing_tomato, ing_onion, ing_parsley, ing_eggs, ing_paprika);
+const Schnitzel         = new Dish("שניצל", ing_eggs, ing_PerureyLehem, ing_chickenbreast, ing_sesame, ing_HardalDizon);
+const StirFriedSalad    = new Dish("מוקפץ", ing_noodles, ing_chickenbreast, ing_carrot, ing_onion, ing_gamba);
+const ThaiStirFried     = new Dish("מוקפץ תאילנדי", ing_chilesouce, ing_soysauce, ing_sesameoil, ing_chicken, ing_gamba);
+const Borsht            = new Dish("בורשט", ing_potato, ing_cabbage, ing_beet, ing_meat, ing_carrot);
+const Yerushalmi        = new Dish("מעורב ירושלמי", ing_spleen, ing_chickenbreast, ing_hawayeg, ing_turmeric, ing_onion);
 
 
     
@@ -103,6 +103,7 @@ const ing_mozzarella    = myPath + "";  // מוצרלה
 const ing_oil           = myPath + "";  // שמן
 const ing_oliveoil      = myPath + "";  // שמן זית
 const ing_onion         = myPath + "";  // בצל
+const ing_noodles       = myPath + "";  // אתריות
 const ing_paprika       = myPath + "";  // פפריקה
 const ing_parmesan      = myPath + "";  // פרמזן
 const ing_parsley       = myPath + "";  // פטרוזיליה
@@ -137,7 +138,6 @@ const ing_yeast         = myPath + "";  // שמרים
     const ing_Humus= myPath + "";// חומוס
     const ing_Bazek_filo= myPath + "";// בצק פילו
     const ing_Baharat= myPath + "";// בהרט
-    const ing_Pasta= myPath + "";// פסטה
     const ing_Hita= myPath + "";// חיתה
     const ing_PerureyLehem= myPath + "";// פירורי לחם
     const ing_HardalDizon= myPath + "";// חרדל דיז'ון
