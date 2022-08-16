d8c4cb9c7600f5f8d8c0f854be0444ff7c912eb1
EitanCher <102179966+EitanCher@users.noreply.github.com>
2022-08-17 02:03:29 +0300

    Arduino compatibility
    Alerts replaced by self-closing messages (hidden divs)
    Input method replaced from clicking to keyboard (AddEventListener updated)

--- Level1.js
+++ Level1.js
@@ -1,13 +1,17 @@
 const numIngreds = 5;
 const numDishes = 5;
+const numQuests = 5;
 var myRndInt;
 var myString;
 var myCorrectAnswer;
 var myQuestions = [];    
 var myIdx;        
 const answerTags = document.getElementsByClassName("btn_answer");
+const myStageIndicators = ["q1", "q2", "q3", "q4", "q5"];
+const myMessageWrong = 'תשובה לא נכונה<br>נסו שוב';
+const myMessageCorrect = 'נכון מאוד!<br>ממשיכים...';
 
-var questions=["q1","q2","q3","q4","q5"];
+
 
 function startLevel1(){
     myRndInt = 0;
@@ -15,22 +19,17 @@ function startLevel1(){
     myCorrectAnswer = 0;
     myIdx = 0;
     myQuestions = getDishes();          // List of (indexes of) Dishes to be asked
-
-    /*
-    let test = "";
-    for (let i =0; i<myQuestions.length; i++){
-        test += `${DishList[myQuestions[i]].DishName} -- `;    
-    }
-    alert (test);
-*/
-    // Arrange the images and answer-options:
-    runQuest(myQuestions[myIdx], myIdx);
     
-    // Respond on player's choice:
-    for (var i = 0; i < answerTags.length; i++) {
-        answerTags[i].addEventListener("click", getAttempt);
-       
-    }  
+    // Display question:
+    runQuest(myQuestions[myIdx], myIdx);
+
+    // Once a button pressed, 
+    // convert its key value into a number ("97" is "a" in ASCII)
+    // and run the function to handle user's repsonse:
+    document.body.addEventListener("keypress", (e) => {
+        const myPressedButton = e.key.charCodeAt(0) - 97;
+        getAttempt(myPressedButton);
+    });
 }
 
  
@@ -40,29 +39,24 @@ function startLevel1(){
 //==============================================================
 
 function runQuest(v, idx) {
-    
-   
-    let s=questions[idx];
-    if(myIdx >= numDishes){
-        alert("Game over");
+
+    const myStageIndicator = myStageIndicators[idx];
+
+    if(myIdx >= numQuests){
+        sendMessage("messageCorrect", "סיימת את השלב בהצלחה!")
+
+
+        // Support theme update in "התקדמות" panel (for the whole list of questions):
         document.getElementById("level1").style.background = " #3f3f3f";
         document.getElementById("lvl1").style.color = "#ffffff";
-      
-       
     } 
     else {
         // 1. Populate the images of the ingredients
-
-        //Update the number of question:
-       
-       
-
-
-       
-       
-
-        document.getElementById(s).style.background = "#d8d513";
-        document.getElementById(s).style.padding = "5px 32.2px";
+        
+        //Change color theme of current stage in left panel:
+        document.getElementById(myStageIndicator).style.background = "#d8d513";
+        document.getElementById(myStageIndicator).style.padding = "5px 32.2px";
+        
         //Clear-up previous images if exist:
         removeAllChildNodes(document.querySelector('#images'));
 
@@ -72,11 +66,6 @@ function runQuest(v, idx) {
                 let myImg = document.createElement("img");
                 myImg.src = DishList[v][k];
                 document.getElementById("images").appendChild(myImg);
-             
-             
-                
-              
-            
             }   
         } 
         
@@ -84,15 +73,15 @@ function runQuest(v, idx) {
 
         // Create array of 4 Dish-Names (including the correct answer)
         myCorrectAnswer = getRndInteger(0, 4); // Store random index 0 to 3 to be used for "correct answer"
-        var myOptions = randAnswers(v, myCorrectAnswer); // Create an array of 4 Dish-names, one of them is "correct"
+        var myOptions = randAnswers(v, myCorrectAnswer);
 
-        // Populate the answer buttons
-        myAnswerButtons = ["Ans_1", "Ans_2", "Ans_3", "Ans_4"]
+        // Populate the answer buttons 
+        // Order of items inside the array matters 
+        myAnswerButtons = ["Ans_4", "Ans_3", "Ans_2", "Ans_1"]
         for (let i = 0; i < myAnswerButtons.length; i++){
             document.getElementById(myAnswerButtons[i]).innerHTML = myOptions[i];
         }
     }
-    
 }
 
 
@@ -104,11 +93,11 @@ function getDishes () {
     var  myDishArray = [];
     
     for (let i = 0; i < numDishes; i++){
-        myRndInt = getRndInteger(0, DishList.length);
+        myRndInt = getRndInteger(0, 40);
         let isUsed = myDishArray.some(myTest1);
 
         while (isUsed) {
-            myRndInt = getRndInteger(0, DishList.length);
+            myRndInt = getRndInteger(0, 40);
             isUsed = myDishArray.some(myTest1);
         }
         myDishArray[i] = myRndInt;
@@ -133,12 +122,12 @@ function randAnswers (correctIndex, myRnd) {
 
     for (let i = 0; i < 4; i++) { 
         if (i != myRnd) {
-            myRndInt = getRndInteger(0, DishList.length);
+            myRndInt = getRndInteger(0, 40);
             myString = DishList[myRndInt].DishName;
             isUsed = myArray.some(myTest2);
 
             while(isUsed || (myRndInt == myRnd)) {
-                myRndInt = getRndInteger(0, DishList.length);
+                myRndInt = getRndInteger(0, 40);
                 myString = DishList[myRndInt].DishName;
                 isUsed = myArray.some(myTest2);
             }
@@ -160,7 +149,7 @@ function randAnswers (correctIndex, myRnd) {
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min) ) + min;
 }
-  
+ 
 function myTest1(v) {
     return v == myRndInt;
 }
@@ -175,25 +164,36 @@ function removeAllChildNodes(parent) {
     }
 }
 
-function getAttempt(){
-    
-   
+function getAttempt(myInput) {
+    if (myInput == myCorrectAnswer) {
+        //Display a message:
+        sendMessage("messageCorrect", myMessageCorrect)
+        
+        //Update Stage Indicator (left panel):
+        const myStageIndicator = myStageIndicators[myIdx];
+        const myStageIndicator_Style = document.getElementById(myStageIndicator).style
 
-    if(this.textContent == DishList[myQuestions[myIdx]].DishName){
-        alert("Correct answer!");
-       
-        
-        
-        let s=questions[myIdx];
-        document.getElementById(s).style.color = "#ffffff";
-        document.getElementById(s).style.background = "#3f3f3f";
-        document.getElementById(s).style.padding = "5px 33.2px";
+        myStageIndicator_Style.color = "#ffffff";
+        myStageIndicator_Style.background = "#3f3f3f";
+        myStageIndicator_Style.padding = "5px 33.2px";
+
+        //Advance to next stage:
         myIdx++;
         runQuest(myQuestions[myIdx], myIdx);
+    }
 
-       
-    }
     else {
-        alert("Wrong answer. Try again.");
+        //Display a message:
+        sendMessage("messageWrong", myMessageWrong)        
     }
+}
+
+function sendMessage(myElementName, myContent){
+    let myMessageElement = document.getElementById(myElementName);
+    myMessageElement.style.display = 'block';
+    myMessageElement.innerHTML = myContent;
+
+    setTimeout(function() {
+        myMessageElement.style.display = 'none';
+    }, 2000);
 }
