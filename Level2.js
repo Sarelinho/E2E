44ff70fbfe9d6f1cf09feb2332ad322882d49d10
EitanCher <102179966+EitanCher@users.noreply.github.com>
2022-08-24 10:41:18 +0300

    Update Level2.js

--- Level2.js
+++ Level2.js
@@ -2,15 +2,7 @@ var isIntro;
 const myIntro = document.getElementById("introduction2");
 
 function startLevel2(){
-    //Callback getAttempt() is summoned here:
-    switchToKeyBoard();
-    isIntro = true;
-
-    myIntro.style.display = 'block';
-    myIntro.style.fontSize = '20px';
-    myIntro.style.backgroundColor = 'blue';
-    myIntro.style.border = 'lightblue 5px solid';
-    myIntro.style.textAlign = 'right';
+    displayIntro();
 }
 
 function respondOnKey(myInput) {
