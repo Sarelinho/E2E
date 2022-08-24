32be2860a74f7e868f6efa84bf3a8c56aac1a3e1
EitanCher <102179966+EitanCher@users.noreply.github.com>
2022-08-24 10:48:25 +0300

    Update Level2.js

--- Level2.js
+++ Level2.js
@@ -1,13 +1,14 @@
-var isIntro;
 const myIntro = document.getElementById("introduction2");
 
 function startLevel2(){
+    
+    // Display Introduction message:
     displayIntro(myIntro);
 }
 
 function respondOnKey(myInput) {
     if(isIntro && myInput == -26) {
-        // "G" key pressed during Intro display:
+        // When "G" key pressed during Intro display, hide Introduction message:
         myIntro.style.display = 'none';
         isIntro = false;
     }
