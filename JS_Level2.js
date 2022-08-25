bb6afb3ea40197494653dc0cdb3493cd9e7baa36
EitanCher <102179966+EitanCher@users.noreply.github.com>
2022-08-25 09:52:15 +0300

    Update JS_Level2.js

--- JS_Level2.js
+++ JS_Level2.js
@@ -7,8 +7,8 @@ function startLevel2(){
 }
 
 function respondOnKey(myInput) {
-    if(isIntro && myInput == -26) {
-        // When "G" key pressed during Intro display, hide Introduction message:
+    if(isIntro && ((myInput == 0) || ((myInput == 3) || ((myInput == 18) || (myInput == 22))))) {
+        // When "a", "s", "d", "w" keys pressed during Intro display, hide Introduction message:
         myIntro.style.display = 'none';
         isIntro = false;
     }
